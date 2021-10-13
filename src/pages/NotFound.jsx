f3c957272374da35843787803b0d8508c25e99b8
brauliorivas <brauliorivas2004@gmail.com>
2021-10-13 17:18:03 -0500

    404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import '../styles/NotFound.scss';
+import { Link } from 'react-router-dom';
+
 
 const NotFound = () => {
-	return (
-		<div>
-			No encontrado, error 404.
-		</div>
-	);
+    return (
+        <div className="errorDialogue">
+            <h1 className="tracking-in-contract-bck-top">¡Error!</h1>
+            <h1 className="focus-in-contract-bck">404</h1>
+            <h3 className="text-shadow-pop-top">Página no encontrada  &#128549;</h3>
+            <Link to="/" className="home-page-link">Regresa a la página de inicio</Link>
+        </div>
+    );
 }
 
 export default NotFound;
